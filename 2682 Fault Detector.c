e6063297968d9f190d1888c381d8c4ab4b1b7d35
OMAR FARUK <umorfarukcse@gmail.com>
2017-10-23 20:25:40 +0600

    Add files via upload

---
+++ 2682 Fault Detector.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a, num[10005], i = 0, j, temp = 0, max = 0;
+	while( (scanf("%d",&num[i])) != EOF ){
+		i++;
+	}
+	max = num[0];
+	for( j = 0; j < i; j++ ){
+		if(num[j] < num[j+1]){
+			max = num[j+1];
+		}
+		else break;
+	}
+	printf("%d\n",max+1);
+	
+	return 0;
+}
